1f0d42fbbc550a29d6ffcb995b2afd09833e7b95
Joshua Emore <ehimareemore@gmail.com>
2023-06-22 15:51:53 -0700

    Changes

--- swap.c
+++ swap.c
@@ -3,7 +3,7 @@
 /**
  ** swap - A function that swaps the top two elements of a stack
  ** @stack: target stack
- ** @line_number: current line command is in
+ ** @line_number: current line command is on
  ** Return: Nothing
  **/
 void swap(stack_t **stack, unsigned int line_number)
