d80ef65664e737213fbc124b2cd60cb5037eaf10
Joshua Emore <ehimareemore@gmail.com>
2023-06-22 15:48:11 -0700

    Changes

--- swap.c
+++ swap.c
@@ -15,7 +15,7 @@ void swap(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	temp = (*stack)->n;
-	(*stack)->n = (*stack)->next->n;
-	(*stack)->next->n = temp;
+	temp = (*stack)->next->n;
+	(*stack)->next->n = (*stack)->n;
+	(*stack)->n = temp;
 }
