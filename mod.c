c02c07c3ddf9f456e7697680dbd8e29246f78d66
Joshua Emore <ehimareemore@gmail.com>
2023-06-22 18:11:17 -0700

    Modulus

--- mod.c
+++ mod.c
@@ -12,10 +12,15 @@ void mod(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	modulus = (*stack)->next->n % (*stack)->n;
 	(*stack)->next->n = modulus;
 	pop(stack, line_number);
